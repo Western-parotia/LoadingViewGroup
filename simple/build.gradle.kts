e1f025671f97908696431ea52958a4645dcc0c05
zhusw <sink50@foxmail.com>
2021-05-06 15:03:53 +0800

    -根目录也使用kts

--- simple/build.gradle.kts
+++ simple/build.gradle.kts
@@ -54,8 +54,8 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
-    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
+    implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
