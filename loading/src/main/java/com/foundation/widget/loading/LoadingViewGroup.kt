cbe39a5f05e5e076fb402b70d4baffb41e03b9f2
zhusw <sink50@foxmail.com>
2021-05-07 10:45:49 +0800

    -loadingView 接口规划

--- loading/src/main/java/com/foundation/widget/loading/LoadingViewGroup.kt
+++ loading/src/main/java/com/foundation/widget/loading/LoadingViewGroup.kt
@@ -1,54 +1,163 @@
 package com.foundation.widget.loading
 
+import android.animation.Animator
 import android.content.Context
-import android.content.res.Resources
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 
 /**
  *@Desc:
- *-
+ *- loading
  *-
  *create by zhusw on 5/6/21 17:08
  */
+private const val ANIM_DURATION = 400L
 
-class LoadingViewGroup(context: Context,attributeSet: AttributeSet?):ViewGroup(context,attributeSet) {
-    constructor(context: Context):this(context,null)
+class LoadingViewGroup(context: Context, attributeSet: AttributeSet?) :
+    ViewGroup(context, attributeSet) {
+    constructor(context: Context) : this(context, null)
 
-   private val undergroundImg = AppCompatImageView(context).apply {
+    var failViewClickListener: FailViewClickListener? = null
+    var loadingAdapter: LoadingAdapter? = null
+        set(value) {
+            field = value
+            postOnAnimation {
+                resetLayout()
+            }
+        }
+
+    private val undergroundImg = AppCompatImageView(context).apply {
         scaleType = ImageView.ScaleType.CENTER_CROP
-        layoutParams = LayoutParams(MATCH_PART,MATCH_PART)
+        layoutParams = LayoutParams(MATCH_PART, MATCH_PART)
         addView(this)
     }
-    private val loadingTextView = TextView(context).apply {
+    private var loadingView: View = TextView(context).apply {
         text = "loading"
         gravity = Gravity.CENTER
-        layoutParams = LayoutParams(WRAP_CONTENT,WRAP_CONTENT)
+        layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
         setBackgroundColor(Color.BLUE)
         addView(this)
+    }
+    private var loadingFailView: View = Button(context).apply {
+        text = "加载失败"
+        gravity = Gravity.CENTER
+        layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        addView(this)
+        visibility = View.INVISIBLE
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         undergroundImg.autoMeasure(this)
-        loadingTextView.autoMeasure(this)
+        loadingView.autoMeasure(this)
+        loadingFailView.autoMeasure(this)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         undergroundImg.autoLayoutToCenter(this)
-        loadingTextView.autoLayoutToCenter(this)
+        loadingView.autoLayoutToCenter(this)
+        loadingFailView.autoLayoutToCenter(this)
     }
 
-    fun setUndergroundImg(id:Int){
-        undergroundImg.setBackgroundResource(id)
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        animation?.cancel()
+        for (i in 0..childCount) {
+            val view = getChildAt(i)
+            view.animation?.cancel()
+        }
     }
-    fun setLoadingTxt(text:String){
-        loadingTextView.text = text
+
+    private fun resetLayout() {
+        removeAllViews()
+        loadingAdapter?.run {
+            getLoadingView()?.let {
+                loadingView = it
+                loadingView.apply {
+                    layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+                    addView(this)
+                }
+            }
+            getLoadingFailView()?.let {
+                loadingFailView = it
+                loadingFailView.apply {
+                    layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+                    visibility = View.INVISIBLE
+                    addView(this)
+                }
+
+            }
+            getLoadingBackground()?.let {
+                undergroundImg.background = it
+            }
+            getLoadingFailEventView()?.setOnClickListener {
+                failViewClickListener?.onClick(it)
+            }
+        }
+
     }
+
+    /**
+     * 整体显示动画 与 loading 动画一起触发，避免间隔
+     * 所以在onShowLoading中最好立即开始执行loading动画
+     */
+    fun showLoading() {
+        alpha = 0F
+        visibility = View.VISIBLE
+        with(loadingFailView) {
+            if (visibility == View.VISIBLE) visibility = View.INVISIBLE
+        }
+        with(loadingView) {
+            if (visibility != View.VISIBLE) visibility = View.VISIBLE
+        }
+        loadingAdapter?.onShowLoading(loadingView)
+        animate().alpha(1F).setDuration(ANIM_DURATION)
+            .start()
+    }
+
+    /**
+     * 整个容器隐藏后回调停止loading
+     */
+    fun stopLoading() {
+        animate().alpha(0F).setDuration(ANIM_DURATION)
+            .setListener(object : AnimatorEndListener() {
+                override fun onEnd(animation: Animator?) {
+                    visibility = View.GONE
+                    loadingAdapter?.onStopLoading(loadingView)
+                }
+            })
+            .start()
+    }
+
+
 }
+
+interface FailViewClickListener {
+    fun onClick(view: View);
+}
+
+private abstract class AnimatorEndListener : Animator.AnimatorListener {
+    override fun onAnimationStart(animation: Animator?) {
+
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+        onEnd(animation)
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {
+    }
+
+    abstract fun onEnd(animation: Animator?)
+}
+
--- simple/src/main/java/com/foundation/widget/simple/MainActivity.kt
+++ simple/src/main/java/com/foundation/widget/simple/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.foundation.widget.simple
 
-import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.foundation.widget.loading.LoadingConfig
 import com.foundation.widget.loading.LoadingViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        val lv = LoadingViewGroup(this)
-//        setContentView(lv)
-//        lv.setUndergroundImg(R.drawable.ic_launcher_background)
-//        lv.setLoadingTxt("加载中。。。。。。。。")
+//        setContentView(R.layout.activity_main)
+        val lv = LoadingViewGroup(this)
+        setContentView(lv)
+//        lv.alpha = 0F
+//        lv.animate().alpha(1F).rotation(350F).setDuration(4000).start()
+
     }
 }
